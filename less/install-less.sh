6911b5cca6a090127d7a943f0805663b9f24b963
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-08-08 10:07:51 +0000

    fix lessc installer and lessc path

--- less/install-less.sh
+++ less/install-less.sh
@@ -2,8 +2,10 @@
 
 # Install Less via npm
 if [ ! -e "$(which lessc)" ]; then
-	sudo npm install -g less
-	sudo npm install -g less-plugin-clean-css
+	# sudo npm install -g less
+	# sudo npm install -g less-plugin-clean-css
+	npm install -g less
+	npm install -g less-plugin-clean-css
 else
     echo LESS $(npm view less version 2>&1 | grep -v WARN) is installed
 fi
