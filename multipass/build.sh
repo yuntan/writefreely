fc1ad32121ae6d34daa92c9115a3bf69697f7b05
Yuto Tokunaga <yuntan.sub1@gmail.com>
2019-08-15 03:23:24 +0000

    add build script using multipass

---
+++ multipass/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+echo "==> install deps"
+sudo snap install go --channel=1.12/stable --classic
+sudo snap install node --channel=12/stable --classic
+sudo apt install -y build-essential sqlite3
+
+echo "==> clone"
+git clone https://github.com/yuntan/writefreely.git
+cd writefreely
+
+echo "==> build"
+export PATH=$PATH:/snap/bin:~/go/bin
+go get github.com/jteeuwen/go-bindata/go-bindata
+GO111MODULE=on make ui build
+mv cmd/writefreely/writefreely .
+
+echo "==> compress"
+fname=writefreely-$(git describe --tags)-linux-x86_64.tar.gz
+tar -cf $fname writefreely pages static templates
+
+echo "==> generated $fname"
---
+++ multipass/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+name=build-writefreely
+multipass launch --name $name --mem 4G 16.04
+multipass copy-files build.sh $name:
+multipass exec $name -- bash build.sh
+multipass copy-files $name:$(multipass exec $name -- find writefreely -maxdepth 1 | grep .tar.gz | tr -d '\r') .
+multipass stop $name
+multipass delete $name
